0508df8633cabf9b74fb18a8ed44cb01568a1fdb
Unknown <miaofch@aliyun.com>
2018-05-11 17:48:56 +0800

    添加图片上传

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class Blog(models.Model):
     description = models.CharField(max_length=100)
+    image = models.ImageField(default='default.png', upload_to='images/')
+    title = models.CharField(default='博客标题', max_length=50)
+
+
+    def __str__(self):
+        return self.title
